8ed74554f351e579c473475d1a93a29e43ab5bea
kie okamoto <y1109k1010@gmail.com>
2025-05-10 13:00:20 +0900

    ER図移動

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c044e4238ee8da199d5a88d3a8c9108e1e616476',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c044e4238ee8da199d5a88d3a8c9108e1e616476',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'laravel-lang/lang' => array(
+            'pretty_version' => '12.17.1',
+            'version' => '12.17.1.0',
+            'reference' => 'b07184103fb64131d514667b8fd1d74c71105409',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel-lang/lang',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
